52eee4908213bfe6d545b9c4fc7715739fed3c0e
Barabazs <31799121+Barabazs@users.noreply.github.com>
2022-04-24 09:10:25 +0200

    Implement IPFS path validation

--- tests/integration/test_is_ipfs.py
+++ tests/integration/test_is_ipfs.py
@@ -1,6 +1,7 @@
-from is_ipfs import Validator
 import unittest
+
 import tests.testing_data as testing_data
+from is_ipfs import Validator
 
 
 class TestCase(unittest.TestCase):
@@ -17,9 +18,8 @@ class TestCase(unittest.TestCase):
                     self.assertFalse(Validator(entry).is_ipfs())
 
         with self.subTest("Test valid IPFS URL entries from fixtures"):
-            for key, value in testing_data.valid_entries["url"].items():
-                for entry in value:
-                    self.assertTrue(Validator(entry).is_ipfs())
+            for entry in testing_data.valid_entries["url"]["ipfs"]:
+                self.assertTrue(Validator(entry).is_ipfs())
 
         with self.subTest("Test invalid IPFS URL entries from fixtures"):
             for entry in testing_data.invalid_entries["url"]["ipfs"]:
@@ -31,6 +31,14 @@ class TestCase(unittest.TestCase):
 
         with self.subTest("Test invalid IPFS subdomain entries from fixtures"):
             for entry in testing_data.invalid_entries["subdomain"]["ipfs"]:
+                self.assertFalse(Validator(entry).is_ipfs())
+
+        with self.subTest("Test valid IPFS path entries from fixtures"):
+            for entry in testing_data.valid_entries["path"]["ipfs"]:
+                self.assertTrue(Validator(entry).is_ipfs())
+
+        with self.subTest("Test invalid IPFS path entries from fixtures"):
+            for entry in testing_data.invalid_entries["path"]["ipfs"]:
                 self.assertFalse(Validator(entry).is_ipfs())
 
 
